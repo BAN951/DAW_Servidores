1bdb877318e9fad214531bcdc169b7098bc96a57
BAN951 <benji.adam.n9@gmail.com>
2019-01-30 22:18:18 +0100

    Stage 2, signup process finished and working. Working on login.

--- ProyectoRegHours/createdb_and_user.sql
+++ ProyectoRegHours/createdb_and_user.sql
@@ -2,11 +2,15 @@
 create database if not exists REGHOURS;
 use REGHOURS;
 
+drop table if exists `TIMERECORD`;
+drop table if exists `USER`;
+
 create table if not exists `USER` (
 	`userId` int not null auto_increment primary key,
 	`username` varchar(32) not null,
     `firstname` varchar(48) not null,
     `lastname` varchar(90) not null, 
+    `email` varchar(68) not null,
     `passwd` varchar(64) not null #sha256 encoded password
 );
 
@@ -21,15 +25,17 @@ create table if not exists `TIMERECORD` (
 
 # passwd: Benjamin995# 
 # --> SHA256: 93A4BEAD3D527E99BC4271330142B2243EC0033BAB74F2F7F94EDDA5FD84659B
-insert into `USER`(`username`, `firstname`, `lastname`, `passwd`)
-values('benji', 'Benjamin Adam', 'Nagy', 
+insert into `USER`(`username`, `firstname`, `lastname`, `email`, `passwd`)
+values('benji', 'Benjamin Adam', 'Nagy', 'benjaminadam.nagy@sonfe.com',
 	   '93A4BEAD3D527E99BC4271330142B2243EC0033BAB74F2F7F94EDDA5FD84659B');
 
 # passwd: Visigodo69@ 
 # --> SHA256: 4B9B1F4BCA3511922128ADC3A5E9509CD24406E0D80D2CF4CCABA8DFACDE9723
-insert into `USER`(`username`, `firstname`, `lastname`, `passwd`)
-values('visigodo', 'Visigodo', 'de Hispania',
+insert into `USER`(`username`, `firstname`, `lastname`, `email`, `passwd`)
+values('visigodo', 'Visigodo', 'de Hispania', 'visigodo.hardcore@sonfe.com',
 	   '4B9B1F4BCA3511922128ADC3A5E9509CD24406E0D80D2CF4CCABA8DFACDE9723');
+       
+       #Note: password for user: Krisz --> Krisztian15#
        
 create user if not exists 'reghours'@'localhost' 
 identified by 'reghourspasswd';
